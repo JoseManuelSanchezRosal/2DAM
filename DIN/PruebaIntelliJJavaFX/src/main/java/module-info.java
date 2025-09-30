731d9ca702f23d74affc3c6870071bae7e38bfa5
JoseManuelSanchezRosal <j.manuel25@outlook.es>
2025-09-30 13:36:16 +0200

    ejercicios y teoria

--- DIN/PruebaIntelliJJavaFX/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.pruebaintellijjavafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.pruebaintellijjavafx to javafx.fxml;
-    exports org.example.pruebaintellijjavafx;
-}
