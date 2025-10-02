a33f0933868802a0f74f4818df54b092c0d6ec75
JoseManuelSanchezRosal <j.manuel25@outlook.es>
2025-10-02 07:42:01 +0200

    ejercicios avanzados PsP

--- PSP/src/avanzadoMultiproceso/ej1/CuentaVocalClase.java
+++ PSP/src/avanzadoMultiproceso/ej1/CuentaVocalClase.java
@@ -4,7 +4,12 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-
+//Crear un programa que sea capaz de contar cuántas vocales hay en un fichero.
+//El programa padre debe lanzar cinco hilos hijo, donde cada uno de ellos se
+//ocupará de contar una vocal concreta (que puede ser minúscula o mayúscula).
+//Cada subproceso que cuenta vocales deberá dejar el resultado en un fichero. El
+//programa padre se ocupará de recuperar los resultados de los ficheros, sumar
+//todos los subtotales y mostrar el resultado final en pantalla.
 public class CuentaVocalClase extends Thread{
     private String entrada;
     private String vocal;
