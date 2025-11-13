1f57811daf1155abacfe3459a844886721b5f081
JoseManuelSanchezRosal <j.manuel25@outlook.es>
2025-11-13 19:22:24 +0100

    Curso SPB v15

--- AaD/SpringBoot/cursoSpringBoot/src/main/java/cursoSpringBoot/controllers/CustomerController.java
+++ AaD/SpringBoot/cursoSpringBoot/src/main/java/cursoSpringBoot/controllers/CustomerController.java
@@ -1,10 +1,7 @@
 package cursoSpringBoot.controllers;
 
 import cursoSpringBoot.domain.Customer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,21 +17,79 @@ public class CustomerController {
             new Customer(234, "Carlos Martinez", "carlosm", "password234")
     ));
 
+    /**
+     * Metodo para devolver lista de clientes
+     * @return lista de clientes
+     */
     @GetMapping("/clientes")
     public List<Customer> getCustomers(){
+
         return customers;
     }
 
+    /**
+     * Metodo para devolver cliente por USERNAME con PathVarible
+     * @param username a devolver
+     * @return cliente
+     */
     @GetMapping("/clientes/{username}")
-    public Customer getCliente(@PathVariable String username){
+    public Customer getCliente(@PathVariable String username) {
         for (Customer c : customers) {
-            if (c.getUserName().equalsIgnoreCase(username)){
+            if (c.getUserName().equalsIgnoreCase(username)) {
+
                 return c;
             }
         }
         return null;
     }
 
+    // Ingresamos tipo de request POST y en parametro ingresamos @RequestBody para decirle a nuestro programa que recibira el JSON con el nuevo cliente
 
+    /**
+     * Metodo para anadir un cliente  con Request body
+     * @param customer anadido
+     * @return cliente
+     */
+    @PostMapping("/clientes")
+    public Customer postCliente(@RequestBody Customer customer){
+        customers.add(customer);
+        return customer;
+    }
 
+    /**
+     * Metodo para modificar un cliente por su id con RequestBody
+     * @param customer cliente a modificar
+     * @return cliente modificado
+     */
+    @PutMapping("/clientes")
+    public Customer putMapping(@RequestBody Customer customer){
+        for(Customer c : customers){
+            if(c.getID() == customer.getID()){
+                c.setName(customer.getName());
+                c.setUserName(customer.getUserName());
+                c.setPassword(customer.getPassword());
+
+                return c;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Metodo para borrar un cliente por su ID con PathVariable
+     * @param id id del cliente a borrar
+     * @return cliente borrado, si lo encuentra, sino devuelve null
+     */
+    @DeleteMapping("/clientes/{id}")
+    public Customer deleteMapping(@PathVariable int id){
+        for (Customer c : customers){
+            if (c.getID() == id){
+                customers.remove(c);
+
+                return c;
+            }
+        }
+        return null;
+    }
 }
+// VIDEO 15
