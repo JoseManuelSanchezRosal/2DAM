6e4c7fab180c3b398756a06012fdd7f602f22441
JoseManuelSanchezRosal <j.manuel25@outlook.es>
2025-11-18 14:41:09 +0100

    Proyecto Spring Boot cine PSP y AaD y ejercicios repaso Examen JavaScript

---
+++ SpringBoot/SpringBoot-cine/src/main/java/com/dam2/Practica1/config/AsyncConfig.java
@@ -0,0 +1,38 @@
+package com.dam2.Practica1.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+    // ThreadPoolExecutor para control de Peliculas
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor ex = new ThreadPoolTaskExecutor();
+        ex.setCorePoolSize(3);
+        ex.setMaxPoolSize(6);
+        ex.setQueueCapacity(20);
+        ex.setThreadNamePrefix("psp-");
+        ex.initialize();
+        return ex;
+    }
+    // Ejercicio 4
+    /**
+     * Configura un pool de hilos para las votaciones del Jurado
+     * @return un Executor configurado con el nombre "threadsJurado" listo para usarse
+     */
+    @Bean(name = "threadsJurado")
+    public ThreadPoolTaskExecutor threadsJurado(){
+
+        ThreadPoolTaskExecutor exJu = new ThreadPoolTaskExecutor();
+        exJu.setCorePoolSize(5); // Maximo de hilos vivos
+        exJu.setMaxPoolSize(10); // Maximo de hilos que se crean si se llena la cola
+        exJu.setQueueCapacity(100); // Capacidad de la cola de espera antes de ser rechazados
+        exJu.setThreadNamePrefix("Thread-"); // Prefijo del hilo
+        exJu.initialize(); // Ejecucion
+        return exJu;
+    }
+}
---
+++ SpringBoot/SpringBoot-cine/src/main/java/com/dam2/Practica1/models/Pelicula.java
@@ -0,0 +1,127 @@
+package com.dam2.Practica1.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "peliculas")
+@Data  // ✅ Lombok genera getters, setters, toString, equals, hashCode
+public class Pelicula {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 120)
+    private String titulo;
+
+    private int duracion;              // minutos
+
+    @Column(name = "fecha_estreno")
+    private LocalDate fechaEstreno;
+
+    private String sinopsis;
+
+    private int valoracion;
+
+    @OneToOne
+    @JoinColumn(name = "ficha_id") // FK en la tabla Pelicula
+    private FichaTecnica fichaTecnica;
+
+    @ManyToOne
+    @JoinColumn(name = "director_id", nullable = false) // FK en PELICULA
+    private Director director;
+
+    @ManyToMany(mappedBy = "peliculas")
+    private List<Actor> actors = new ArrayList<>();
+
+    public Pelicula(Long id, String titulo, int duracion, LocalDate fechaEstreno, String sinopsis, int valoracion, FichaTecnica fichaTecnica, Director director, List<Actor> actors) {
+        this.id = id;
+        this.titulo = titulo;
+        this.duracion = duracion;
+        this.fechaEstreno = fechaEstreno;
+        this.sinopsis = sinopsis;
+        this.valoracion = valoracion;
+        this.fichaTecnica = fichaTecnica;
+        this.director = director;
+        this.actors = actors;
+    }
+
+    public Pelicula() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public LocalDate getFechaEstreno() {
+        return fechaEstreno;
+    }
+
+    public void setFechaEstreno(LocalDate fechaEstreno) {
+        this.fechaEstreno = fechaEstreno;
+    }
+
+    public String getSinopsis() {
+        return sinopsis;
+    }
+
+    public void setSinopsis(String sinopsis) {
+        this.sinopsis = sinopsis;
+    }
+
+    public int getValoracion() {
+        return valoracion;
+    }
+
+    public void setValoracion(int valoracion) {
+        this.valoracion = valoracion;
+    }
+
+    public FichaTecnica getFichaTecnica() {
+        return fichaTecnica;
+    }
+
+    public void setFichaTecnica(FichaTecnica fichaTecnica) {
+        this.fichaTecnica = fichaTecnica;
+    }
+
+    public Director getDirector() {
+        return director;
+    }
+
+    public void setDirector(Director director) {
+        this.director = director;
+    }
+
+    public List<Actor> getActors() {
+        return actors;
+    }
+
+    public void setActors(List<Actor> actors) {
+        this.actors = actors;
+    }
+}
